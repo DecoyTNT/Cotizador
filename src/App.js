d9e08e2dd5e1be493c2874618dba7c691a056d25
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-04-04 01:36:38 -0600

    Terminado hasta leer datos del formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+    background-color: #FFF;
+    padding: 3rem;
+`;
 
 function App() {
-    return <Header
-        titulo="Cotizador de seguros"
-    />;
+
+    return (
+        <Contenedor>
+            <Header
+                titulo="Cotizador de seguros"
+            />
+
+            <ContenedorFormulario>
+                <Formulario />
+            </ContenedorFormulario>
+        </Contenedor>
+    );
 }
 
 export default App;
